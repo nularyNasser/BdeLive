32a72fe4e6daf8eae00d7041f96f6d70cca3783c
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-09-18 00:57:28 +0200

    Test php web site

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>My Project Game</title>
+    </head>
+    <body>
+        Welcome World !
+        I'm Nasser.
+    </body>
+</html>
