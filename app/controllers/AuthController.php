0ee9ddba5bf2407fe08b113ace53e91b479f3071
BOUDHIB-Mohame-Amine-24006551 <mohamed-amine.boudhib@etu.univ-amu.fr>
2025-10-02 16:22:38 +0200

    Modiification du AuthController.php

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,23 +1,21 @@
 <?php
-if (isset($_POST['ok'])) { // notre bouton a t-il été cliqué
-    //si oui on recupere les données suivants
-    $nom = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+//if (isset($_POST['ok'])) { // notre bouton a t-il été cliqué
+//si oui on recupere les données suivants
 
-    //on se connecte a la base de donnée
-    $servername = "localhost";
-    $username = "root";
-    $dbpassword = "";
-    $dbname = "utilisateurs";
 
-    try{
-        $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $dbpassword);
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e){
-        echo "Connection echouer: " . $e->getMessage();
-    }
+//on se connecte a la base de donnée
+$servername = "mysql-boudhib.alwaysdata.net";
+$username = "429915";
+$dbpassword = "bdelive+6";
+$dbname = "bdelivesae_db";
 
+try{
+    $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $dbpassword);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch(PDOException $e){
+    echo "Connexion échouer: " . $e->getMessage();
+}
+
+//}
 ?>
