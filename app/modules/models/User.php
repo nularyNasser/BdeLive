e78d5e6141b0bed6afac2229fbfa73d4eb4598f2
BOUDHIB-Mohame-Amine-24006551 <mohamed-amine.boudhib@etu.univ-amu.fr>
2025-10-03 08:55:24 +0200

    Création de la fonction register. Version 1.1

--- app/modules/models/User.php
+++ app/modules/models/User.php
@@ -1,8 +1,32 @@
 <?php
 class User {
     public function __construct(private $id, private $username, private $email, private $password) {
-        database::connect();
-    }
+            database::connect();
+        }
 
+    public function register() {
+        try {
+            $stmt = $this->connection->prepare('INSERT INTO Inscription (username, email, password) VALUES(?, ?, ?)');
+
+            if (!$stmt) {
+                throw new Exception("Erreur lors de la préparation de la requête");
+            }
+            $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT);
+
+            $stmt->bind_param("sss", $this->username, $this->email, $hashedPassword);
+
+            if ($stmt->execute()) {
+                $stmt->close();
+                return true;
+            } else {
+                $stmt->close();
+                throw new Exception("Erreur lors de l'insertion : " . $stmt->error);
+            }
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            return false;
+        }
+    }
 }
 ?>
