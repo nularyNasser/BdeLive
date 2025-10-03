1bcce872c1a480d5516a6a2619ba1780c7c6a7f5
romain <romain.cantor13@gmail?com>
2025-10-03 09:07:36 +0200

    Ajout du lien 'Pas de compte'

--- app/modules/views/loginPageView.php
+++ app/modules/views/loginPageView.php
@@ -6,12 +6,13 @@
     <h1>Connexion</h1>
     <form id="form" action="index.php?page=home" method="POST">
         <label for="email">Adresse e-mail</label>
-        <input id="email" type="text" name="Adresse e-mail" value=" "> <br>
+        <input id="email" type="text" name="Adresse e-mail" placeholder="Entrez votre Adresse mail" required> <br>
         <label for="password">Password</label>
-        <input id="password" type="text" name="Password" value=" ">
+        <input id="password" type="text" name="Password" placeholder="Entrez votre mot de passe" required>
     </form>
     <br>
     <p><a href="index.php?page=home">Revenir à l'accueil</a></p>
     <p><a href="index.php?page= ">Mot de passe oublié ?</a></p>
+    <p><a href="index.php?page=register">Pas de compte?</a></p>
 
 <?php end_page(); ?>
