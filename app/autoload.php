d9161c6ea9fdd60bdb636e9a33283bb8319161bf
amin Helali <amin.helali@etu.univ-amu.fr>
2025-10-04 15:00:18 +0200

    autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -1,3 +1,16 @@
 <?php
-// Chargement automatique des classes à implémenter
+
+    spl_autoload_register(function($className){
+        $paths = [
+            __DIR__ . '/modules/models/' . $className . '.php',
+            __DIR__ . '/modules/controllers/' . $className . '.php',
+        ];
+
+        foreach ($paths as $path) {
+            if (file_exists($path)) {
+                require_once $path;
+                return;
+            }
+        }
+    });
 ?>
