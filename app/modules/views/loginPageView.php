2e1be8c8ba8de835f8cb77676d96838fc12f9199
romain <romain.cantor13@gmail?com>
2025-10-05 14:39:23 +0200

    Ajout CSS login

--- app/modules/views/loginPageView.php
+++ app/modules/views/loginPageView.php
@@ -3,17 +3,23 @@
     start_page("Login");
 ?>
 
+<link rel="stylesheet" href="./assets/css/login.css">
+
+<div class="login-container">
     <h1>Connexion</h1>
     <form id="form" action="index.php?page=home" method="POST">
         <label for="email">Adresse e-mail</label>
         <input id="email" type="text" name="Adresse e-mail" placeholder="Entrez votre Adresse mail" required> <br>
+
         <label for="password">Password</label>
         <input id="password" type="text" name="Password" placeholder="Entrez votre mot de passe" required> <br>
-        <input type="submit" value="Se connecter" name="ok">
-    </form>
-    <br>
-    <p><a href="index.php?page=home"> <-- Retour à l'accueil</a></p>
-    <p><a href="index.php?page= ">Mot de passe oublié ?</a></p>
-    <p><a href="index.php?page=register">Pas de compte?</a></p>
 
-<?php end_page(); ?>
+        <input type="submit" value="Se connecter" name="ok">
+
+        <p><a href="index.php?page=forgotPassword">Mot de passe oublié ?</a></p>
+    </form>
+</div>
+
+<?php
+    end_page();
+?>
