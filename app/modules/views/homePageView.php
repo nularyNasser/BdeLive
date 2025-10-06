94f7c1e78fae4061e5088e8d3542f60276750006
nularyNasser <nahamed.sveil@gmail.com>
2025-10-06 09:24:48 +0200

    Changement des chemins des images

--- app/modules/views/homePageView.php
+++ app/modules/views/homePageView.php
@@ -39,13 +39,13 @@
                 </ul>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="./assets/img/event1.png" alt="Premier événement" width="800" height="400" loading="lazy">
+                        <img class="d-block w-100" src="assets/img/event1.png" alt="Premier événement" width="800" height="400" loading="lazy">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="./assets/img/event2.png" alt="Deuxième événement" width="800" height="400" loading="lazy">
+                        <img class="d-block w-100" src="assets/img/event2.png" alt="Deuxième événement" width="800" height="400" loading="lazy">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="./assets/img/event3.png" alt="Troisième événement" width="800" height="400" loading="lazy">
+                        <img class="d-block w-100" src="qassets/img/event3.png" alt="Troisième événement" width="800" height="400" loading="lazy">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
